d574f8d7e465293173082f8d73aae8ec2f88eb64
Jasonjunsu <hegq5891@gmail.com>
2017-03-27 22:07:07 +0800

    add the new algorithm of finding the new area

---
+++ Java_Practice/src/com/github/jason/alogrithim/FindReaAreaA.java
@@ -0,0 +1,58 @@
+package com.github.jason.alogrithim;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+class FindReaAreaA{
+
+	static int N;
+	static int[][] data =new int[27][27];
+	static int dem,max=0;
+
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		// sc = new Scanner(new FileInputStream("C:\\Users\\sev_user\\workspace\\thuchanhDay2\\src\\SkyMap\\input.txt"));
+
+		int T = sc.nextInt();
+		for (int test_case = 0; test_case < T; test_case++) {
+			int i, j,C=0;
+			N = sc.nextInt();
+			
+			
+			for (i = 1; i <=N; i++) {
+				for (j = 1; j <=N; j++) {
+					data[i][j] = sc.nextInt();
+				}
+			}
+			dem=0;
+			for (i = 1; i <=N; i++)
+				for (j = 1; j <= N; j++)
+					if (data[i][j] == 0) {
+						dem++;
+						max=0;
+						expand(i, j);
+						if(max>C) C=max;
+					}
+
+			// Print the answer to standard output(screen).
+			System.out.println(dem + " " + C);
+		}
+	}
+
+	private static void expand(int i, int j) {
+		// TODO Auto-generated method stub
+		if (data[i][j] == 0) {
+			System.out.println(i + " " +j);
+			max++;
+			data[i][j] = 1;
+			if (i > 1)
+				expand(i - 1, j);
+			if (j > 1)
+				expand(i, j - 1);
+			if (i < N)
+				expand(i + 1, j);
+			if (j < N)
+				expand(i, j + 1);
+		}
+		return;
+	}
+}
